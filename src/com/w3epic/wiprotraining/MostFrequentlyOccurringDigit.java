82f258048f1d94e94fefe2e5bb1212be14dbe8dc
Arpan Das <arp14@yahoo.com>
2018-08-21 21:36:53 +0530

    Passed all test cases.

--- src/com/w3epic/wiprotraining/MostFrequentlyOccurringDigit.java
+++ src/com/w3epic/wiprotraining/MostFrequentlyOccurringDigit.java
@@ -9,21 +9,18 @@ class MostFrequentlyOccurringDigit {
 
 	public int mostFrequentlyOccurringDigit(int[] input1,int input2){
 		// Read only region end
-		//if (input2 < 0) input2 *= -1;
-		//System.out.println(Arrays.toString(input1));
 		StringBuilder input = new StringBuilder();
-		
 		for (int ip : input1) input.append(ip);
-		
+
 		int[] freq = new int[10];
-		
+
 		/*for (int i = 0; i < input1.length; i++) {
 			int number = input1[i];
 			if (number < 0) number *= -1;
-			
+
 			// {0, -1, 12300, 11, 002} 5
 			if (number == 0) freq[0]++;
-			
+
 			while (number != 0) {
 				freq[number % 10]++;
 				System.out.print((number % 10) + ", ");
@@ -33,15 +30,18 @@ class MostFrequentlyOccurringDigit {
 		for (int j = 0; j < input.length(); j++) {
 			freq[Integer.parseInt(String.valueOf(input.charAt(j)))]++;
 		}
+
+        	int maxFreqIndex = 0;
+		int maxFreq = 0;
 		
-		int maxFreqIndex = 0;
 		for (int i = 9; i >= 0; i--) {
-			//maxFreqIndex = freq[i] > freq[maxFreqIndex] ? i : maxFreqIndex;
-			if (freq[i] > freq[maxFreqIndex]) maxFreqIndex = i;
+			if (freq[i] > maxFreq) {
+				maxFreqIndex = i;
+				maxFreq = freq[i];
+			}
 		}
 		
-		//System.out.println(Arrays.toString(freq) + " max i: " + maxFreqIndex);
-		
+		//System.out.println(Arrays.toString(freq) + " max i: " + maxFreqIndex);			
 		return maxFreqIndex;
 	}
 }
